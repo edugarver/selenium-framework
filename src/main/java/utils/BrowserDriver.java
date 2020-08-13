19646534647b291c4980948f7995032b79372b96
Eduardo Verdugo <everdugo@securityscorecard.io>
2020-08-13 16:49:16 +0200

    quick fix for drivers

--- src/main/java/utils/BrowserDriver.java
+++ src/main/java/utils/BrowserDriver.java
@@ -11,44 +11,43 @@ import java.util.logging.Logger;
  * Class used to manage the webdriver during the execution of the test
  */
 public class BrowserDriver {
-    private static final Logger LOGGER = Logger.getLogger(BrowserDriver.class.getName());
-    private static WebDriver driver;
+  private static final Logger LOGGER = Logger.getLogger(BrowserDriver.class.getName());
+  private static WebDriver driver;
 
-    public synchronized static WebDriver getCurrentDriver() {
-        if (driver==null) {
-            try {
-//                System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
-                System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-                driver = new ChromeDriver();
-                driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-            } finally {
-                Runtime.getRuntime().addShutdownHook(new Thread(new BrowserCleanup()));
-            }
-        }
-        return driver;
+  public synchronized static WebDriver getCurrentDriver() {
+    if (driver == null) {
+      try {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/mac/chromedriver");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+      } finally {
+        Runtime.getRuntime().addShutdownHook(new Thread(new BrowserCleanup()));
+      }
     }
+    return driver;
+  }
 
-    private static class BrowserCleanup implements Runnable {
-        public void run() {
-            close();
-        }
+  private static class BrowserCleanup implements Runnable {
+    public void run() {
+      close();
     }
+  }
 
-    public static void close() {
-        try {
-            getCurrentDriver().quit();
-            driver = null;
-            LOGGER.info("closing the browser");
-        } catch (UnreachableBrowserException e) {
-            LOGGER.info("cannot close browser: unreachable browser");
-        }
+  public static void close() {
+    try {
+      getCurrentDriver().quit();
+      driver = null;
+      LOGGER.info("closing the browser");
+    } catch (UnreachableBrowserException e) {
+      LOGGER.info("cannot close browser: unreachable browser");
     }
+  }
 
-    public static void loadPage(String url){
-        getCurrentDriver();
-        LOGGER.info("Directing browser to:" + url);
-        LOGGER.info("try to loadPage [" + url + "]");
-        getCurrentDriver().get(url);
-    }
+  public static void loadPage(String url) {
+    getCurrentDriver();
+    LOGGER.info("Directing browser to:" + url);
+    LOGGER.info("try to loadPage [" + url + "]");
+    getCurrentDriver().get(url);
+  }
 
 }
